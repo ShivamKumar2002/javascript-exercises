6fbacdaab95cfa788bb69555256fc2f553ffbaf7
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2022-10-24 18:01:20 +0530

    Solve Exercise 7 - Temperature Conversion
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let inFahrenheit = (temperature - 32) * (5 / 9)
+  return Math.round(inFahrenheit * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let inCelsius = (temperature * 9 / 5) + 32;
+  return Math.round(inCelsius * 10) / 10;
 };
 
 // Do not edit below this line
