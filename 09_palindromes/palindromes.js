353bcfe43a1aae8530efc4ba46986b23c1f17406
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2022-10-31 21:20:13 +0530

    Solve Exercise 9 - Palindromes
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    s = s.toLowerCase();
+    s = s.replaceAll(/[.,\s!]/ig, "");
+    for (let i = 0; i < Math.floor(s.length / 2); i++) {
+        if (s[i] !== s[s.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
